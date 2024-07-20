ca95e43371553f6ed9f869f9cf9c9ad7ec1d6091
Benson-chou <benson.chou@mail.utoronto.ca>
2024-07-20 15:39:01 +0800

    can retrieve the checked items now

--- home/home.js
+++ home/home.js
@@ -55,33 +55,33 @@ const items = [
 router.get('/', function(request, response) {
     // Get the items list here!
     // First step: Get the current date and check if there are any results that include this date
-    let today = new Date().toISOString().slice(0, 10)
-    // We need to find a place to clear the older deals!!!
-    const checkquery = 'SELECT * FROM items WHERE valid_from <= ? AND valid_to >= ?'
-    connection.query(checkquery, [today, today], function(err, res){
-        if (err) throw err;
-        // If there isn't, then we call the python file 
-        if (results.length = 0){
-            // Clear the table first
-            const clearquery = 'TRUNCATE TABLE items'
-            connection.query(clearquery, function(err, res){
-                if (err) throw err;
-            })
-            // Run the python script to load table items with newest deals
-            const python = spawn('python', ['../scrape_items.py']);
-            python.on('close', (code) => {
-                console.log('child process close all stdio with code: ${code}');
-            })
-        }
-        // Then we pull items from the database
-        const getquery = 'SELECT * FROM items'
-        connection.query(getquery, function(err, res){
-            if (err) throw err;
-            if (results.length > 0){
-                const items = res;
-            }
-        })
-    })
+    // let today = new Date().toISOString().slice(0, 10)
+    // // We need to find a place to clear the older deals!!!
+    // const checkquery = 'SELECT * FROM items WHERE valid_from <= ? AND valid_to >= ?'
+    // connection.query(checkquery, [today, today], function(err, res){
+    //     if (err) throw err;
+    //     // If there isn't, then we call the python file 
+    //     if (results.length = 0){
+    //         // Clear the table first
+    //         const clearquery = 'TRUNCATE TABLE items'
+    //         connection.query(clearquery, function(err, res){
+    //             if (err) throw err;
+    //         })
+    //         // Run the python script to load table items with newest deals
+    //         const python = spawn('python', ['../scrape_items.py']);
+    //         python.on('close', (code) => {
+    //             console.log('child process close all stdio with code: ${code}');
+    //         })
+    //     }
+    //     // Then we pull items from the database
+    //     const getquery = 'SELECT * FROM items'
+    //     connection.query(getquery, function(err, res){
+    //         if (err) throw err;
+    //         if (results.length > 0){
+    //             const items = res;
+    //         }
+    //     })
+    // })
 
 
 	// If the user is loggedin
@@ -104,15 +104,17 @@ router.get('/', function(request, response) {
 });
  
 // Get the location informations
-router.post('/location', (req, res) => {
-    const location = req.body;
-    
-})
+// router.post('/location', (req, res) => {
+//     const location = req.body;
+// })
 
 // Get the parameters and items selected
 // Get the selected items
+var checkednames = null;
+router.post('/process_items', (req, res) => {
+    console.log(req.body.itemCheckbox)
+})
 
 // Get the parameters
-
 
 module.exports = router;
