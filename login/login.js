8695c78ded46ca69c1af55e74a1f7a3f5594dcce
etheliachoi <etheliachoi@gmail.com>
2024-08-24 16:53:50 -0400

    Parse JSON file to avoid syntax error

--- login/login.js
+++ login/login.js
@@ -12,12 +12,11 @@ const { GoogleAuth } = require('google-auth-library');
 const credentials64 = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 console.log(credentials64);
 if (credentials64) {
-    // const credentials = JSON.parse(
-    //     Buffer.from(credentials64, 'base64').toString('utf8')
-    // );
     const path = '/tmp/credentials.json'
-    const credentials = Buffer.from(credentials64, 'base64').toString('utf8');
-    fs.writeFileSync(path, credentials, 'utf8');
+    const credentials = JSON.parse(
+        Buffer.from(credentials64, 'base64').toString('utf8')
+    );
+    fs.writeFileSync(path, JSON.stringify(credentials), 'utf8');
     process.env.GOOGLE_APPLICATION_CREDENTIALS = path;
     const auth = new GoogleAuth({
         keyFile: path,
