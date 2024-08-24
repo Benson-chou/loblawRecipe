9308663809cd5b1957dd0f12ecb0dd5c9974db18
etheliachoi <etheliachoi@gmail.com>
2024-08-24 16:34:26 -0400

    Try using without writing to file again

--- home/home.js
+++ home/home.js
@@ -14,16 +14,16 @@ const { GoogleAuth } = require('google-auth-library');
 
 const credentials64 = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 if (credentials64) {
-    // const credentials = JSON.parse(
-    //     Buffer.from(credentials64, 'base64').toString('utf8')
-    // );
-    const path = '/tmp/credentials.json'
-    const credentials = Buffer.from(credentials64, 'base64').toString('utf8');
-    fs.writeFileSync(path, credentials, 'utf8');
-    const auth = new GoogleAuth({
-        credentials: path,
-        scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-    });
+    const credentials = JSON.parse(
+        Buffer.from(credentials64, 'base64').toString('utf8')
+    );
+    // const path = '/tmp/credentials.json'
+    // const credentials = Buffer.from(credentials64, 'base64').toString('utf8');
+    // fs.writeFileSync(path, credentials, 'utf8');
+    // const auth = new GoogleAuth({
+    //     credentials: path,
+    //     scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+    // });
   } else {
     console.error('GOOGLE_APPLICATION_CREDENTIALS is not set.');
   }
